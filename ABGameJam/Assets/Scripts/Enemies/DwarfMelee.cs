08c2523bb02419a8013e52d7f87ecb166870929b
IanWark <wark@ualberta.ca>
2019-08-24 11:47:21 -0600

    Add dwarf melee enemy spawning

--- ABGameJam/Assets/Scripts/Enemies/DwarfMelee.cs
+++ ABGameJam/Assets/Scripts/Enemies/DwarfMelee.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DwarfMelee : MonoBehaviour
 {
+    public float spawnHeight = -1.05f;
+
     // Start is called before the first frame update
     void Start()
     {
--- ABGameJam/Assets/Scripts/SpawnController.cs
+++ ABGameJam/Assets/Scripts/SpawnController.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     // Can spawn a new thing every newSpawnSize units
+    public float spawnDistanceFromCamera = 5;
     public float newSpawnSize = 5;
     private float lastXPositionSpawned = 0;
+
+    public DwarfMelee dwarfMelee;
 
     // Update is called once per frame
     void Update()
     {
-        if (Globals.mainCamera.transform.position.x >= lastXPositionSpawned + newSpawnSize)
-        {
+        float cameraX = Globals.mainCamera.transform.position.x;
 
+        if (cameraX >= lastXPositionSpawned + newSpawnSize)
+        {
+            Instantiate(dwarfMelee, new Vector2(cameraX + spawnDistanceFromCamera, dwarfMelee.spawnHeight), gameObject.transform.rotation);
+
+
+
+            lastXPositionSpawned = Globals.mainCamera.transform.position.x;
         }        
     }
 }
