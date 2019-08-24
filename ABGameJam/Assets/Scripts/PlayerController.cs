ac1ae3176bbfb4598da10b43aa84418e6ae068f0
Alexander Ozero <a.ozero@live.com>
2019-08-24 11:41:34 -0600

    Add beginnings of health system to player

--- ABGameJam/Assets/Scripts/PlayerController.cs
+++ ABGameJam/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth = 1000;
+    
     public float speed = 5;
 
     public bool controlEnabled = true;
+    
+    private int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set health to maximum
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,5 +29,12 @@ public class PlayerController : MonoBehaviour
             // Simple x-axis only movement
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0));
         }
+    }
+    
+    public void OnHit(int damage)
+    {
+        currentHealth -= damage;
+
+        healthSlider.value = currentHealth;
     }
 }
