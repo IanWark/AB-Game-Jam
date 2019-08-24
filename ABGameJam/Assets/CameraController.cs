a23b30f88217c6b45b6d912e2776b5b02d0b3cb1
IanWark <wark@ualberta.ca>
2019-08-24 09:37:37 -0600

    Add player and camera that move indepently of each other

---
+++ ABGameJam/Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 5;
+
+    public bool controlEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controlEnabled)
+        {
+            bool right = Input.GetKey(KeyCode.L);
+            bool left = Input.GetKey(KeyCode.J);
+
+            int direction = (right ? 1 : 0) - (left ? 1 : 0);
+
+            // Simple x-axis only movement
+            transform.Translate(new Vector3(direction * speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
---
+++ ABGameJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    public bool controlEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controlEnabled)
+        {
+            // This may be useful if we want to make the movement better: https://roystan.net/articles/character-controller-2d.html
+
+            // Simple x-axis only movement
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
